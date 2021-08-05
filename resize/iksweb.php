f77fe555f93078fc06714ae74090a8436ee0bf69
IKSWEB <46905310+Center-WD@users.noreply.github.com>
2021-08-05 22:15:56 +0300

    Update iksweb.php
    Update script

--- resize/iksweb.php
+++ resize/iksweb.php
@@ -10,57 +10,27 @@
 header("Content-type: image/png; text/html; charset=utf-8;");
 require __DIR__ . '/include/class.php';
 
-/*
-== Using == 
+$RImages = new ResizeImages; // init class
 
-https://yoursite.com/[dir]]/[size]]/[link]
+$RImages->arParams['SAVE_DIR'] = '/resize/images/'; // Дериктория для сохранения картинок
+$RImages->arParams['SHOW_STUB'] = 'Y'; // Отображать ли картинку заглушки для отсутсвующих ищображений
 
-https://yoursite.com/resize/800x800/https://yoursite.com/images.png
-
-
-== Settings ==
-
-You can change the user settings for the individual operation of the script
-
-*/
-$arParams = array(
-    // default
-    'ROOT'	=> __DIR__, // Site ROOT DIR
-    'PAGE'	=> $APPLICATION->GetCurPage(), // Page URL
-    'QUERU' => $APPLICATION->GetCurPageParam(), // Page URL Query
-    'SOURCE'=> $APPLICATION->GetCurSource(),// The source image
-
-    // User Settings
-    'SAVE_DIR' => '/images/', // Folder to save image resizes to (Relative to the main folder)
-    'SHOW_STUB' => 'Y', // Y / N - Display the stub when there is no image
-);
-
-$arParams['IMAGES_PARAMS'] = $APPLICATION->GetResizeParams();
-
-$arParams['FILE_DIR'] = $arParams['ROOT'].$arParams['SAVE_DIR'].$arParams['IMAGES_PARAMS']['FILE_RESIZE_NAME'];
+// Дериктория для сохранения
+$RImages->arParams['FILE_DIR'] = $RImages->arParams['ROOT'].$RImages->arParams['SAVE_DIR'].$RImages->arParams['IMAGES_PARAMS']['FILE_RESIZE_NAME'];
 
 // Show images
-if (file_exists($arParams['FILE_DIR'])) {
+if (file_exists($RImages->arParams['FILE_DIR'])) {
 
-    // Set 200
-    http_response_code(200);
+    http_response_code(200);  // Set 200
 
-    $APPLICATION->load($arParams['FILE_DIR']);
-    $APPLICATION->resize($arParams['IMAGES_PARAMS']['WIDTH'], $arParams['IMAGES_PARAMS']['HEIGHT']);
-    $APPLICATION->output();
+    $RImages->ShowImages(); // Показываем
 
 }else{
 
-    // Set 404
-    http_response_code(404);
+    http_response_code(404); // Set 404
 
-    $APPLICATION->save( $arParams['SOURCE'].$arParams['QUERU'] , $arParams['FILE_DIR'] );
+    $RImages->SaveImages(); // Сохраняем
+    $RImages->ShowImages(); // Показываем
 
-    // We output the absence of an image
-    if($arParams['SHOW_STUB']=='Y'){
-        $APPLICATION->load($arParams['ROOT'].'/include/no-image.png');
-        $APPLICATION->resize($arParams['IMAGES_PARAMS']['WIDTH'], $arParams['IMAGES_PARAMS']['HEIGHT']);
-        $APPLICATION->output();
-    }
 }
 ?>
